ee6b06a488e309b2c6f9ed9e556e32a54834df9a
thegeekkid <brian@geekkidconsulting.com>
2016-12-23 22:03:16 -0600

    Add icon

--- ProPsync-CoreGUI/ProPsync-CoreGUI/Form1.Designer.cs
+++ ProPsync-CoreGUI/ProPsync-CoreGUI/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -46,6 +47,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 62);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "ProPsync";
             this.Load += new System.EventHandler(this.Form1_Load);
